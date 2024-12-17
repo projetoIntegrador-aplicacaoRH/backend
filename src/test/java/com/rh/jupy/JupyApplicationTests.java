6e9d0f3c7b2ce2674cd1651868f2568455e51ee3
adanisantana <mariano.dani@hotmail.com>
2024-12-17 11:09:12 -0300

    configuração do projeto

---
+++ src/test/java/com/rh/jupy/JupyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rh.jupy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JupyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
