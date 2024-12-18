c44df45a2066234cad40992579d0ad4bc98fac3a
rodrigo oliveira <pracimaestudos@gmail.com>
2024-12-18 11:39:28 -0300

    base security

--- src/main/java/com/rh/jupy/repository/UsuarioRepository.java
+++ src/main/java/com/rh/jupy/repository/UsuarioRepository.java
@@ -1,5 +1,7 @@
 package com.rh.jupy.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +10,5 @@ import com.rh.jupy.model.Usuario;
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
 
-	
+	public Optional<Usuario> findByUsuario(String usuario);
 }
